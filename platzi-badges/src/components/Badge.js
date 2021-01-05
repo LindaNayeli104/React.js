7453eb469f2212d8c3b17e6507f941920718acdd
LindaNayeli104 <linda_nayeli104@hotmail.com>
2021-01-05 09:50:16 -0600

    added more functionalities

--- platzi-badges/src/components/Badge.js
+++ platzi-badges/src/components/Badge.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-import confLogo from "../images/badge-header.svg";
-
 import "./styles/Badge.css";
+import confLogo from "../images/badge-header.svg";
+import Gravatar from "./Gravatar";
 
 class Badge extends React.Component {
   render() {
@@ -13,11 +13,7 @@ class Badge extends React.Component {
         </div>
 
         <div className="Badge__section-name">
-          <img
-            className="Badge__avatar"
-            src={this.props.avatarUrl}
-            alt="Avatar"
-          />
+          <Gravatar className="Badge__avatar" email={this.props.email} />
           <h1>
             {this.props.firstName} <br /> {this.props.lastName}
           </h1>
@@ -28,7 +24,7 @@ class Badge extends React.Component {
           <div>@{this.props.twitter}</div>
         </div>
 
-        <div className="Badge__footer">##platziconf</div>
+        <div className="Badge__footer">#platziconf</div>
       </div>
     );
   }
